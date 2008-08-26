756dac2de9a3f3900224dab040f367e83ce01d2a
William Xu <william.xwl@gmail.com>
2008-08-26 21:49:28 +0800

    Release 1.0.

--- cal-china-x.el
+++ cal-china-x.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2006, 2007, 2008 William Xu
 
 ;; Author: William Xu <william.xwl@gmail.com>
-;; Version: 1.0a
+;; Version: 1.0
 ;; Url: http://williamxu.net9.org/ref/cal-china-x.el
 
 ;; This program is free software; you can redistribute it and/or modify
